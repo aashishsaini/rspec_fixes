e58bad3283f0c5cfb51f30d455100b4f2e0c2d2f
Aashish Saini <aashusaini2684@gmail.com>
2023-08-28 14:11:37 -0400

    fixed failing rspecs

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,9 @@ class Enrollment < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   belongs_to :teacher, foreign_key: :teacher_id, class_name: 'User'
   belongs_to :program
+
+  scope :all_teacher_ids, -> { map(&:teacher_id).compact }
+  scope :all_student_ids, -> { map(&:user_id).compact }
+
+  scope :favorites, -> { where(favorite: true).map(&:teacher) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,46 @@
 class User < ApplicationRecord
+  has_many :enrollments
+
+  enum kind: {
+    student_kind: 0,
+    teacher_kind: 1,
+    user_kind: 2
+  }
+
+  scope :classmates, -> (user) {
+    program_ids, teacher_ids = [], []
+    user.enrollments.each{ |record| program_ids << record.program_id; teacher_ids << record.teacher_id }
+
+    includes(:enrollments).distinct.where(enrollments: { program_id: program_ids.uniq, teacher_id: teacher_ids.uniq })
+                       .where.not(enrollments: { user_id: user.id })
+  }
+
+  validate :validate_kind, on: :update
+
+  def teachers
+    enrollments
+  end
+
+  def teacher?
+    self.kind == 'teacher_kind'
+  end
+
+  def student?
+    self.kind == 'student_kind'
+  end
+
+  private
+
+  def validate_kind
+    case self.kind
+    when 'student_kind'
+      if Enrollment.all_teacher_ids.include?(self.id)
+        self.errors.add(:kind, "can not be student because is teaching in at least one program")
+      end
+    when 'teacher_kind'
+      if Enrollment.all_student_ids.include?(self.id)
+        self.errors.add(:kind, "can not be teacher because is studying in at least one program")
+      end
+    end
+  end
 end
